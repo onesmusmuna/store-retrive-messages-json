dd05398a417ea29894cb489c98e3f42baf9e9138
onesmus muna <onesmusjones@gmail.com>
2021-11-25 09:57:59 +0300

    validating requests with pipes

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dto/create-message.dto';
 
 @Controller('messages')
 export class MessagesController {
@@ -6,8 +7,12 @@ export class MessagesController {
   getMessages() {}
 
   @Post()
-  createMessage() {}
+  createMessage(@Body() body: CreateMessageDto) {
+    console.log(body);
+  }
 
   @Get('/:id')
-  getMessageById() {}
+  getMessageById(@Param('id') id: string) {
+    console.log(id);
+  }
 }
